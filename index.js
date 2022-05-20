d5094d3fb3b65b0b666eb3a962a6848c64250854
printf83 <printf83@gmail.com>
2022-05-20 14:34:08 +0800

    update check render time

--- index.js
+++ index.js
@@ -2,6 +2,23 @@
 let result = [];
 let max = 0;
 
+function addResizeListener(elem, fun) {
+	let id;
+	let style = getComputedStyle(elem);
+	let wid = style.width;
+	let hei = style.height;
+	id = requestAnimationFrame(test);
+	function test() {
+		let newStyle = getComputedStyle(elem);
+		if (wid !== newStyle.width || hei !== newStyle.height) {
+			fun();
+			wid = newStyle.width;
+			hei = newStyle.height;
+		}
+		id = requestAnimationFrame(test);
+	}
+}
+
 function showStart() {
 	document.getElementById(
 		"root"
@@ -11,9 +28,7 @@ function showStart() {
 function startCalc() {
 	max = parseInt(document.getElementById("max").value);
 
-	document.getElementById("root").innerHTML = `Calculating prime inside <b>${max.toLocaleString(
-		"en-US"
-	)}</b> numbers...`;
+	document.getElementById("root").innerHTML = `Find prime number in <b>${max.toLocaleString("en-US")}</b> numbers...`;
 
 	setTimeout(function () {
 		let min = 1;
@@ -53,26 +68,36 @@ function startCalc() {
 	}, 1);
 }
 
+let render_start = 0;
+let render_end = 0;
+
 function showResult() {
 	document.getElementById("root").innerHTML = `Please wait. Generating <b>${max.toLocaleString(
 		"en-US"
 	)}</b> result into your browser...`;
 
-	let start = window.performance.now();
-	setTimeout(function () {
-		document.getElementById("root").innerHTML = `
-        <button onclick="showStart()">Try Again</button><br/>
-        <small id="speed_label1"></small>
-        <div class="d-flex">${result.join(`</div><div class="d-flex">`)}</div>
-        <small id="speed_label2"></small>
-        <button onclick="showStart()">Try Again</button>`;
-
-		let end = window.performance.now();
-		let genLength = (end - start).toFixed(1).toLocaleString("en-US");
+	render_start = window.performance.now();
+	let root = document.getElementById("root");
+	addResizeListener(root, function () {
+		render_end = window.performance.now();
+		let genLength = parseFloat((render_end - render_start).toFixed(1)).toLocaleString("en-US");
 
 		document.getElementById("speed_label1").innerHTML = `This list generated in ${genLength}ms`;
 		document.getElementById("speed_label2").innerHTML = `This list generated in ${genLength}ms`;
-	}, 1);
+	});
+
+	setTimeout(
+		function (root) {
+			root.innerHTML = `
+        <button onclick="showStart()">Try Again</button><br/>
+        <small id="speed_label1"></small>
+        <div class="d-flex">${result.join(`</div><div class="d-flex">`)}</div>
+        <small id="speed_label2"></small><br/>
+        <button onclick="showStart()">Try Again</button>`;
+		},
+		1,
+		root
+	);
 }
 
 function isPrime(num, arrayOfPrime) {
@@ -94,6 +119,7 @@ function isPrime(num, arrayOfPrime) {
 }
 
 function factor(num, arrayOfPrime) {
+	//return factor1(num);
 	return factor4(num, arrayOfPrime);
 }
 
