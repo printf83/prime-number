5ee0afc66fc6ca2f7af679fb228bb22a4859df24
printf83 <printf83@gmail.com>
2022-05-30 17:20:51 +0800

    single prime update

--- singleprime.js
+++ singleprime.js
@@ -1,14 +1,17 @@
 onmessage = function (e) {
 	try {
 		let max = e.data[0];
+		let max2 = parseInt(max / 2,10);
 		let min = 1;
 		let result = [];
 
-		for (let x = min; x <= max; x++) {
+		for (let x = min; x <= max2; x++) {
 			if (max % x === 0) {
 				result.push(x);
 			}
 		}
+
+		result.push(max)
 
 		postMessage(result);
 	} catch (err) {
