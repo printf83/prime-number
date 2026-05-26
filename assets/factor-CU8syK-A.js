2d9b3bc3e5b9039128be57b61dbcb04649c0cb14
Hamzah A <printf83@gmail.com>
2026-05-26 21:05:47 +0800

    Updates

---
+++ assets/factor-CU8syK-A.js
@@ -0,0 +1,8 @@
+(function(){function e(e,t=100){let n=t<=0?1:t;return e>n?Math.max(1,Math.floor(e/n)):1}function t(e){let t=-1;return function(n,r,i){if(i!==0){if(n===r){t!==100&&(t=100,e(100));return}if(n%i===0){let i=Math.floor(n/r*100);t!==i&&(t=i,e(i))}}}}function n(e){return e===0||e?e.toLocaleString(`en-US`):`Error!`}(function(){let r=t(e=>{postMessage({type:`progress`,data:e})});function i(t,r,i,a){let o=[],s=t.length>2?Math.floor(t.length/2):t.length,c=i===1?e(s):0;for(let e=0;e<s;e++)o.push(`
+                    <tr>
+                        <td>÷</td>
+                        <td>${n(t[e])}</td>
+                        <td>=</td>
+                        <td>${n(r/t[e])}</td>
+                    </tr>
+                `),i===1&&a(e,s,c);return`<tbody>${o.join(``)}</tbody>`}self.onmessage=function(e){try{let[t,n,a,o]=e.data,s=null;s=a===0?t.join(`, `).replace(/, ((?:.(?!, ))+)$/,` and $1`):i(t,n,o,r),postMessage({type:`data`,data:s})}catch(e){postMessage({type:`error`,error:String(e&&typeof e==`object`&&`message`in e?e.message:e)})}}})()})();
